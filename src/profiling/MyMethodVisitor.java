6ccf5f3275c7f5eece2e100ac5a8c15fa27f420b
Fabian Hasselbach <hasselba@infcip57.stud.informatik.uni-trier.de>
2017-12-12 16:34:54 +0100

    profiling with asm

---
+++ src/profiling/MyMethodVisitor.java
@@ -0,0 +1,27 @@
+package profiling;
+
+
+import org.objectweb.asm.MethodVisitor;
+import org.objectweb.asm.Opcodes;
+import org.objectweb.asm.commons.AdviceAdapter;
+
+/**
+ * Created by hasselba on 12.12.17.
+ */
+public class MyMethodVisitor extends AdviceAdapter implements Opcodes {
+    protected MyMethodVisitor(int api, MethodVisitor mv, int access, String name, String desc) {
+        super(api, mv, access, name, desc);
+    }
+    @Override
+    public void visitMethodInsn(int opcode, String owner, String name,
+                                String desc, boolean itf) {
+        mv.visitFieldInsn(GETSTATIC, "java/lang/System", "err", "Ljava/io/PrintStream;");
+        mv.visitLdcInsn("CALL " + name);
+        mv.visitMethodInsn(INVOKEVIRTUAL, "java/io/PrintStream",
+                "println", "(Ljava/lang/String;)V", false);
+
+        System.out.println(opcode + owner + name);
+
+        super.visitMethodInsn(opcode, owner, name, desc, itf);
+    }
+}
---
+++ src/profiling/RaytracerAgent.java
@@ -0,0 +1,11 @@
+package profiling;
+
+import java.lang.instrument.Instrumentation;
+/**
+ * Created by hasselba on 12.12.17.
+ */
+public class RaytracerAgent {
+    public static void premain(String agentArgs, Instrumentation inst) {
+        inst.addTransformer(new RaytracerTransformer());
+    }
+}
